da9721f092fc572b230e648dc9e50605e79cb6a2
pimtegelaar <pimtegelaar@gmail.com>
2017-08-21 11:40:21 +0200

    updating 014

---
+++ src/main/java/com/tegeltech/mutationtestingexperiment/PrimeFinder014.java
@@ -0,0 +1,12 @@
+package com.tegeltech.mutationtestingexperiment;
+
+public class PrimeFinder014 {
+  public boolean isPrime(double n) {
+    for (int i = 2; 2 * i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
