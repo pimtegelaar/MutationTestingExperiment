223a41eb39e22d2dcd03abe3a5ecb5d93ab887a9
pimtegelaar <pimtegelaar@gmail.com>
2017-08-24 00:06:38 +0200

    increase test length 10

--- src/test/java/com/tegeltech/mutationtestingexperiment/PrimeFinder000Test.java
+++ src/test/java/com/tegeltech/mutationtestingexperiment/PrimeFinder000Test.java
@@ -25,8 +25,8 @@ public class PrimeFinder000Test {
 //        boolean result = primeFinder000.isPrime(573259433d);
 //        boolean result = primeFinder000.isPrime(654188429d);
 //        boolean result = primeFinder000.isPrime(735632797d);
-        boolean result = primeFinder000.isPrime(817504253d);
-//        boolean result = primeFinder000.isPrime(899809363d);
+//        boolean result = primeFinder000.isPrime(817504253d);
+        boolean result = primeFinder000.isPrime(899809363d);
     assertThat(result, is(true));
   }
 }
